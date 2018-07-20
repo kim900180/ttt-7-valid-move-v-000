808d293db750efc47b636724b23e17ad7f90b034
Jae Yong Kim <kim900180@gmail.com>
2018-07-20 20:41:08 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -13,7 +13,7 @@ end
 def position_taken?(board, index)
   if board[index] == " " || board[index] == ""
     false
-  if board[index] == "X" || board[index] == "O"
+  elsif board[index] == "X" || board[index] == "O"
     true
   end
 end
